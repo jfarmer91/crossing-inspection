0f4fd9efa6cf39dac269a98681e25560da529677
jfarmer91 <farmerjong@gmail.com>
2016-01-28 09:54:51 -0500

    Pinch Zoom Smaller Increments

--- js/photo.js
+++ js/photo.js
@@ -13,55 +13,51 @@ function getParameterByName(name) {
 
 var photoUrl = getParameterByName("url");
 
-document.getElementById("info").innerHTML = "<img src='" + photoUrl + "' id='image' alt='site-image' width='100%' onclick='zoom(this)'>";
+document.getElementById("info").innerHTML = "<img src='" + photoUrl + "' id='image' alt='site-image' width='100%'>";
 
 
 
-function zoom (x) {
-  var previousWidth = document.getElementById("save-old-width").innerHTML;
-
-  console.log(previousWidth);
-  var width = x.width;
-  document.getElementById("save-old-width").innerHTML = width;
-  console.log(previousWidth);
-  console.log(width);
-
-  var widthRatio = ( width / previousWidth );
-  console.log(widthRatio);
-
-  if ( previousWidth === "" ) {
-    x.style.width = "200%";
-  } else if ( widthRatio === 2 ) {
-    x.style.width = "300%";
-  } else if ( widthRatio === 1.5 ) {
-    console.log("eh");
-    x.style.width = "200%";
-  } else if ( widthRatio === (2/3) ) {
-    x.style.width = "100%";
-  } else if (widthRatio === 0.5 ) {
-    x.style.width = "200%";
-  }
-}
+// function zoom (x) {
+//   var previousWidth = document.getElementById("save-old-width").innerHTML;
+//
+//   console.log(previousWidth);
+//   var width = x.width;
+//   document.getElementById("save-old-width").innerHTML = width;
+//   console.log(previousWidth);
+//   console.log(width);
+//
+//   var widthRatio = ( width / previousWidth );
+//   console.log(widthRatio);
+//
+//   if ( previousWidth === "" ) {
+//     x.style.width = "200%";
+//   } else if ( widthRatio === 2 ) {
+//     x.style.width = "300%";
+//   } else if ( widthRatio === 1.5 ) {
+//     console.log("eh");
+//     x.style.width = "200%";
+//   } else if ( widthRatio === (2/3) ) {
+//     x.style.width = "100%";
+//   } else if (widthRatio === 0.5 ) {
+//     x.style.width = "200%";
+//   }
+// }
 
 var pictureDiv = document.getElementById("info");
 
 var hammertime = new Hammer(pictureDiv);
 
-hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+// hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 hammertime.get('pinch').set({ enable: true});
 
-hammertime.on('pinch pinchin pinchout panleft panright panup pandown tap press', function(ev) {
+hammertime.on('pinch pinchin pinchout tap', function(ev) {
   console.log(ev.type + "whatever");
-  // if (ev.type === "pinch") {
-  //   MouseWheelHandler();
-  //   alert("wow");
-  // }
   document.getElementById("pinch-event").innerHTML = "<h3>" + ev.type + "</h3>";
   if (ev.type === "pinchin") {
-    image.style.width = Math.max(200, Math.min(4320, image.width - 100)) + "px";
+    image.style.width = Math.max(200, Math.min(4320, image.width - 10)) + "px";
   } else if (ev.type === "pinchout") {
-    image.style.width = Math.max(200, Math.min(4320, image.width + 100)) + "px";
+    image.style.width = Math.max(200, Math.min(4320, image.width + 10)) + "px";
   }
 });
 
