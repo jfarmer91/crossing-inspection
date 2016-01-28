56da204be399c43a4bb223f6eaf9d88e11b35f6d
jfarmer91 <farmerjong@gmail.com>
2016-01-28 14:11:11 -0500

    Test jQuery animation

--- js/photo.js
+++ js/photo.js
@@ -42,77 +42,89 @@ document.getElementById("info").innerHTML = "<img src='" + photoUrl + "' id='ima
 //     x.style.width = "200%";
 //   }
 // }
-// var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-//
-//
-// var pictureDiv = document.getElementById("info");
-//
-// var hammertime = new Hammer(pictureDiv);
-//
-// hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-// hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-// hammertime.get('pinch').set({ enable: true});
-//
-// hammertime.on('pinch pinchstart pinchin pinchout pan panstart panleft panright panup pandown swipeup swipedown swipeleft swiperight tap', function(ev) {
-//
-//   document.getElementById("pinch-event").innerHTML = "<h3>" + ev.type + window.event + "</h3>";
-//   if (ev.type === "panstart") {
-//     console.log(ev);
-//   }
-//
-//   // if (ev.type === "pinchin") {
-//   //   image.style.width = Math.max(width, Math.min(4320, image.width - 25)) + "px";
-//   // } else if (ev.type === "pinchout") {
-//   //   image.style.width = Math.max(width, Math.min(4320, image.width + 10)) + "px";
-//   // } else if (ev.type === "swipeleft") {
-//   //   console.log(ev);
-//   //   // console.log(ev.target.x);
-//   //   // console.log(ev.center);
-//   //   console.log(ev.pointers[0].screenX);
-//   //   console.log(ev.deltaX);
-//   //   console.log((ev.pointers[0].screenX - ev.deltaX));
-//   //
-//   //   var scrollDistance = Math.abs(ev.target.x) + Math.abs(ev.target.width * 0.25 * ev.overallVelocity);
-//   //   $("#info").animate({
-//   //     scrollLeft: scrollDistance
-//   //   }, 500)
-//   //
-//   // } else if (ev.type === "swiperight") {
-//   //
-//   //   console.log(ev.target.x);
-//   //
-//   //   var scrollDistance = Math.abs(ev.target.x) - Math.abs(ev.target.width * 0.25 * ev.overallVelocity);
-//   //   $("#info").animate({
-//   //     scrollLeft: scrollDistance
-//   //   }, 500)
-//   //
-//   // } else if (ev.type === "swipeup") {
-//   //   console.log(ev.target.y);
-//   //
-//   //   var scrollDistance = Math.abs(ev.target.y) + Math.abs(ev.target.height * 0.25 * ev.overallVelocity);
-//   //   $("#info").animate({
-//   //     scrollTop: scrollDistance
-//   //   }, 500)
-//   //
-//   //
-//   // } else if (ev.type === "swipedown") {
-//   //   console.log(ev.target.y);
-//   //
-//   //   var scrollDistance = Math.abs(ev.target.y) - Math.abs(ev.target.height * 0.25 * ev.overallVelocity);
-//   //   $("#info").animate({
-//   //     scrollTop: scrollDistance
-//   //   }, 500)
-//   //
-//   // } else if (ev.type === "panright") {
-//   //   pictureDiv.scrollLeft -= 1;
-//   // } else if (ev.type === "panleft") {
-//   //   pictureDiv.scrollLeft += 1;
-//   // } else if (ev.type === "panup") {
-//   //   pictureDiv.scrollTop += 1;
-//   // } else if (ev.type === "pandown") {
-//   //   pictureDiv.scrollTop -= 1;
-//   // }
-// });
+var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+
+var pictureDiv = document.getElementById("info");
+
+var hammertime = new Hammer(pictureDiv);
+
+hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+hammertime.get('pinch').set({ enable: true});
+
+hammertime.on('pinch pinchstart pinchin pinchout pan panstart panleft panright panup pandown swipeup swipedown swipeleft swiperight tap', function(ev) {
+
+  document.getElementById("pinch-event").innerHTML = "<h3>" + ev.type + window.event + "</h3>";
+  if (ev.type === "panstart") {
+    console.log(ev);
+    ev.target.x = "500";
+    console.log(ev.target.x);
+  }
+
+  if (ev.type === "pinchin") {
+    // image.style.width = Math.max(width, Math.min(4320, image.width - 25)) + "px";
+    imageWidth = Math.max(width, Math.min(4320, image.width - 500)) + "px";
+
+    $("#info").animate({
+      width: imageWidth
+    }, 500)
+  } else if (ev.type === "pinchout") {
+    // image.style.width = Math.max(width, Math.min(4320, image.width + 10)) + "px";
+    imageWidth = Math.max(width, Math.min(4320, image.width + 250)) + "px";
+
+    $("#info").animate({
+      width: imageWidth
+    }, 500)
+  } else if (ev.type === "swipeleft") {
+    console.log(ev);
+    // console.log(ev.target.x);
+    // console.log(ev.center);
+    console.log(ev.pointers[0].screenX);
+    console.log(ev.deltaX);
+    console.log((ev.pointers[0].screenX - ev.deltaX));
+
+    var scrollDistance = Math.abs(ev.target.x) + Math.abs(ev.target.width * 0.25 * ev.overallVelocity);
+    $("#info").animate({
+      scrollLeft: scrollDistance
+    }, 500)
+
+  } else if (ev.type === "swiperight") {
+
+    console.log(ev.target.x);
+
+    var scrollDistance = Math.abs(ev.target.x) - Math.abs(ev.target.width * 0.25 * ev.overallVelocity);
+    $("#info").animate({
+      scrollLeft: scrollDistance
+    }, 500)
+
+  } else if (ev.type === "swipeup") {
+    console.log(ev.target.y);
+
+    var scrollDistance = Math.abs(ev.target.y) + Math.abs(ev.target.height * 0.25 * ev.overallVelocity);
+    $("#info").animate({
+      scrollTop: scrollDistance
+    }, 500)
+
+
+  } else if (ev.type === "swipedown") {
+    console.log(ev.target.y);
+
+    var scrollDistance = Math.abs(ev.target.y) - Math.abs(ev.target.height * 0.25 * ev.overallVelocity);
+    $("#info").animate({
+      scrollTop: scrollDistance
+    }, 500)
+
+  } else if (ev.type === "panright") {
+    pictureDiv.scrollLeft -= 1;
+  } else if (ev.type === "panleft") {
+    pictureDiv.scrollLeft += 1;
+  } else if (ev.type === "panup") {
+    pictureDiv.scrollTop += 1;
+  } else if (ev.type === "pandown") {
+    pictureDiv.scrollTop -= 1;
+  }
+});
 
 var image = document.getElementById("image");
 
